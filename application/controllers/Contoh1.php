5b88186921566c5f622cd398fa4e975b38e02c21
sirpai <nurrahmanfakhri@gmail.com>
2023-09-20 21:29:05 +0700

    menambahkan Contoh1.php untuk Tugas Pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Fakhri Nurrahman
+            saya tinggal di Bekasi
+            olahraga yang saya sukai adalah
+            tidak ada :) ";
+    }
+}
