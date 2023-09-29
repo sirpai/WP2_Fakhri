b5b627de4885e097231a0eba2b70ff0062afcb32
sirpai <nurrahmanfakhri@gmail.com>
2023-09-29 11:01:51 +0700

    Menambahkan tugas pertemuan 2 'Latihan1.php, Model-latihan1.php, view-latihan1.php' ;

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Latihan 1 - Fakhri</title>
+    </head>
+    <body>
+       <h1>Halo Kawan... Yuk Kita belajar web programming...!!!</h1><br>
+        Nilai 1 = <?= $nilai1; ?>
+        Nilai 2 = <?= $nilai2; ?>
+        ini hasil dari permodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    </body>
+</html>
