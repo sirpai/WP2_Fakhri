9df0a7bcc7780a6966935db09ea4965dc8c96af1
sirpai <nurrahmanfakhri@gmail.com>
2023-10-17 20:28:08 +0700

    Tugas Pertemuan 3

---
+++ application/controllers/v_footer.php
@@ -0,0 +1,6 @@
+            <footer>
+                <a href="https://www.Rentalbuku.com">Rentalbuku</a>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ application/controllers/v_header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Programming II | Merancang Template sederhana dengan CodeIgniter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?> assets/css/stylebuku.css">
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <hgroup>
+                <h1>Rentalbuku.net</h1>
+                <h3>Membuat Template sederhana dengan CodeIgniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
