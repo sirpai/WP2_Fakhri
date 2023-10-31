5ece9e6acb82f89e13ba3191e99a0ff697aea548
sirpai <nurrahmanfakhri@gmail.com>
2023-10-31 20:53:01 +0700

    Tugas Pertemuan 7

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('form_validation', 'session', 'database');
 
 /*
 | -------------------------------------------------------------------
@@ -70,7 +70,7 @@ $autoload['libraries'] = array();
 | offer multiple interchangeable driver options.
 |
 | Prototype:
-|
+    |
 |	$autoload['drivers'] = array('cache');
 |
 | You can also supply an alternative property name to be assigned in
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('ModelUser', 'ModelBuku');
